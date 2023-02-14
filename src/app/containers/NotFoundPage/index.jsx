cfe8b4f6765a759f97bcadf04a2e66d49a6f9f00
Aleksandar Peev <alexpeev9@abv.bg>
2023-02-14 16:24:05 +0200

    Add not found page

--- src/app/containers/NotFoundPage/index.jsx
+++ src/app/containers/NotFoundPage/index.jsx
@@ -1,5 +1,10 @@
 const NotFoundPage = () => {
-  return <div>Page not found</div>
+  return (
+    <main class='d-flex flex-column align-items-center justify-content-center pt-2 text-dark'>
+      <h1 class='display-1 fw-bold'>404</h1>
+      <h2>Page not found</h2>
+    </main>
+  )
 }
 
 export default NotFoundPage
