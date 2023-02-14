946ebf9f82ee887398dd5ea3bffbcc908bb24a88
Aleksandar Peev <alexpeev9@abv.bg>
2023-02-14 18:00:54 +0200

    Fix class to className

--- src/app/containers/App/Header.jsx
+++ src/app/containers/App/Header.jsx
@@ -5,14 +5,13 @@ import './assets/style.css'
 
 const Header = () => {
   return (
-    <header class='d-flex justify-content-around align-items-center bg-dark text-white'>
-      <img src={Logo} alt='logo' height='80' width='80' />
-      <Link
-        class='d-flex align-items-center position-relative p-2 rounded-circle'
-        to='/'
-      >
-        <i class='bi bi-handbag text-white'></i>
-        <span class='position-absolute top-0 end-0 bg-warning text-white rounded-circle badge p-2'>
+    <header className='d-flex justify-content-around align-items-center bg-dark text-white'>
+      <Link to='/'>
+        <img src={Logo} alt='logo' height='80' width='80' />
+      </Link>
+      <Link className='d-flex align-items-center position-relative px-3' to='/'>
+        <i className='bi bi-handbag text-white'></i>
+        <span className='position-absolute top-0 end-0 bg-warning text-white rounded-circle badge p-2'>
           +1
         </span>
       </Link>
--- src/app/containers/NotFoundPage/index.jsx
+++ src/app/containers/NotFoundPage/index.jsx
@@ -1,7 +1,7 @@
 const NotFoundPage = () => {
   return (
-    <main class='d-flex flex-column align-items-center justify-content-center pt-2 text-dark'>
-      <h1 class='display-1 fw-bold'>404</h1>
+    <main className='d-flex flex-column align-items-center justify-content-center pt-2 text-dark'>
+      <h1 className='display-1 fw-bold'>404</h1>
       <h2>Page not found</h2>
     </main>
   )
