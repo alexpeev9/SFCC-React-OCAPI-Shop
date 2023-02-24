367c65779a653d3a72cd1db3e69dbc188c3f9f1e
Aleksandar Peev <alexpeev9@abv.bg>
2023-02-24 13:56:55 +0200

    Refactor ContentAsset Hook

--- src/hooks/useGetContentAsset.jsx
+++ src/hooks/useGetContentAsset.jsx
@@ -1,7 +1,30 @@
-import useFetch from './useFetch'
+import { useEffect, useState } from 'react'
+import { useTokenContext } from '../contexts/TokenContext'
+import { fetchData } from '../services/fetchService'
 
 const useGetContentAsset = (id) => {
-  return useFetch('GET', `/content/${id}`)
+  const { token } = useTokenContext()
+  const [data, setData] = useState(null)
+  const [error, setError] = useState(null)
+
+  useEffect(() => {
+    const fetch = async () => {
+      if (token) {
+        const { ok, data } = await fetchData(token, {
+          method: 'GET',
+          url: `/content/${id}`
+        })
+        if (ok) {
+          setData(data)
+        } else {
+          setError(data.message)
+        }
+      }
+    }
+    fetch()
+  }, [token, id])
+
+  return { data, error }
 }
 
 export default useGetContentAsset
